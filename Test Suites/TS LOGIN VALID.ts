222fd9adf3f6756167658eb052d6c2453244ab72
riotamar <rio.tamara@sahassa.co.id>
2023-05-23 13:34:39 +0700

    Add SAMPLE TEST SUITE LOGIN

---
+++ Test Suites/TS LOGIN VALID.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>WEB PAGE and LOGIN VALID</description>
+   <name>TS LOGIN VALID</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>45758603-f168-4de4-8636-1f250234b9c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c667a0e-5ea8-4a40-ad07-007e74f18bc1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC.REGLA.LOGIN.001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d216033b-a34d-4433-a874-08d3a4fb3c08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC.REGLA.LOGIN.003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
